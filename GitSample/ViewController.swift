9c37bcd22f5c3bb29d313f970b87b08e2be44f11
H-CLo <nba1895@gmail.com>
2018-09-10 23:08:51 +0800

    First commit

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by Hung Chang Lo on 2018/9/10.
+//  Copyright © 2018年 Hung Chang Lo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // FIXME: Leaking memory
+    
+    // TODO: -
+    // TODO: Refactor data source and navigation
+    // TODO: Need to localize strings
+    // TODO: -
+    
+    func qqq() -> Void {
+        
+    }
+
+}
+
